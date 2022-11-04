a862076e048c8967419f7a0fde869b12db91d0d2
Salvatore Rappoccio <rappoccio@gmail.com>
2022-11-04 16:25:06 -0400

    Adding a nanov9 response maker that vaguely works with the selection and adds histograms correctly. Not done with the actual response matrix yet.

---
+++ inputfiles_dymc.py
@@ -0,0 +1,3 @@
+eras = ['UL16NanoAOD', 'UL16NanoAODAPV', 'UL17NanoAOD', 'UL18NanoAOD']
+htbins = ['70to100', '100to200', '200to400', '400to600', '600to800', '800to1200', '1200to2500', '2500toInf']
+filestr = '/mnt/data/cms/store/mc/RunIISummer20%sv9/DYJetsToLL_M-50_HT-%s_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1*/*/*.root
