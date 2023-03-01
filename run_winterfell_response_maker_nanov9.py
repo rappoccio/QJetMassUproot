d6c8d4c918794de2edfa26dac679885efcc5271d
Salvatore Rappoccio <rappoccio@gmail.com>
2023-03-01 10:27:19 -0500

    Adding a command line run driver

---
+++ run_winterfell_response_maker_nanov9.py
@@ -0,0 +1,10 @@
+from smp_utils import *
+from response_maker_nanov9_lib import *
+from response_maker_nanov9 import *
+
+def main():
+    
+    response_maker_nanov9(client=None, do_gen=True, testing=False, prependstr="/mnt/data/cms/")
+    
+if __name__ == "__main__":
+    main()
